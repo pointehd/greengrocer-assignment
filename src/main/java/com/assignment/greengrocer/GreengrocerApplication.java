c62709e2ecac6eb8b20490328772a97e74a3367e
Donghyun Hwang <pointehd@naver.com>
2023-01-28 08:52:15 +0900

    Init: spring boot

---
+++ src/main/java/com/assignment/greengrocer/GreengrocerApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.greengrocer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreengrocerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreengrocerApplication.class, args);
+	}
+
+}
