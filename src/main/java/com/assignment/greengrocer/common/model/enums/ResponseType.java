dc1cb9cbf3951c94adfe5e67b4944d8a792449b3
Donghyun Hwang <pointehd@naver.com>
2023-01-28 18:42:00 +0900

    Feat: Response 객체 및 ResponseType enum 추가

---
+++ src/main/java/com/assignment/greengrocer/common/model/enums/ResponseType.java
@@ -0,0 +1,9 @@
+package com.assignment.greengrocer.common.model.enums;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum ResponseType {
+    SUCCESS
+    ;
+}
---
+++ src/main/java/com/assignment/greengrocer/common/model/out/Response.java
@@ -0,0 +1,25 @@
+package com.assignment.greengrocer.common.model.out;
+
+import com.assignment.greengrocer.common.model.enums.ResponseType;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Response<T> {
+
+    private ResponseType responseType;
+    private T data;
+
+    public static Response<String> error(ResponseType responseType, String errorMessage) {
+        return new Response(responseType, errorMessage);
+    }
+
+    public static <T> Response<T> success(ResponseType responseType, T data) {
+        return new Response(responseType, data);
+    }
+
+}
