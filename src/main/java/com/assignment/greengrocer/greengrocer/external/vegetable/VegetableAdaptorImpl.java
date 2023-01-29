33b776a560e2805710633d681bb77b7b9ef25def
Donghyun Hwang <pointehd@naver.com>
2023-01-29 15:40:24 +0900

    Docs: 채소 api 연동
    - header cookie 가져오는 코드 추가
    - 채소 api 연동

--- src/main/java/com/assignment/greengrocer/greengrocer/external/vegetable/VegetableAdaptorImpl.java
+++ src/main/java/com/assignment/greengrocer/greengrocer/external/vegetable/VegetableAdaptorImpl.java
@@ -5,17 +5,25 @@ import com.assignment.greengrocer.greengrocer.external.ExternalService;
 import com.assignment.greengrocer.greengrocer.external.GreengrocerProperties;
 import com.assignment.greengrocer.greengrocer.external.GreengrocerType;
 import com.assignment.greengrocer.greengrocer.model.PriceResponse;
+import java.util.Arrays;
 import java.util.List;
 import javax.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
 
+@Slf4j
 @Component
 public class VegetableAdaptorImpl implements ExternalAdaptor {
 
     private final WebClient webClient;
     private final ExternalService externalService;
+
+    private static final String HEADER_COOKIE = "Authorization=";
+    private static final String AUTH_HEADER = "Authorization";
 
 
     VegetableAdaptorImpl(
@@ -36,16 +44,40 @@ public class VegetableAdaptorImpl implements ExternalAdaptor {
 
     @Override
     public String getToken() {
-        return null;
+        ResponseEntity<String> responseCookie = webClient.get()
+            .uri("/token")
+            .retrieve()
+            .toEntity(String.class)
+            .block();
+        log.debug("Response token Header: {}", responseCookie);
+        return getHeaderToken(responseCookie);
+    }
+
+    private <T> String getHeaderToken(ResponseEntity<T> response) {
+        String responseCookie = response.getHeaders()
+            .get(HttpHeaders.SET_COOKIE)
+            .get(0);
+        return responseCookie.substring(HEADER_COOKIE.length(), responseCookie.indexOf(";"));
     }
 
     @Override
     public List<String> getCategory() {
-        return null;
+        return Arrays.stream(webClient.get()
+                .uri("/item")
+                .headers(httpHeaders -> httpHeaders.add(AUTH_HEADER, getToken()))
+                .retrieve()
+                .bodyToMono(String[].class)
+                .block())
+            .toList();
     }
 
     @Override
     public PriceResponse getPrice(String name) {
-        return null;
+        return webClient.get()
+            .uri(uriBuilder -> uriBuilder.path("/item").queryParam("name", name).build())
+            .headers(httpHeaders -> httpHeaders.add(AUTH_HEADER, getToken()))
+            .retrieve()
+            .bodyToMono(PriceResponse.class)
+            .block();
     }
 }
