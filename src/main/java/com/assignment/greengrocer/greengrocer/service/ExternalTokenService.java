4b0e3af621bbff3779b670d9e36e0ad2f5454acd
Donghyun Hwang <pointehd@naver.com>
2023-01-30 16:12:53 +0900

    Feat: 비동기 코드 삭제
    - 비동기 코드 삭제
    - 토큰 만료시 비동기 코드로 인해 코드 복잡도 상승
    - api 응닶 속도에 큰 영향을 주지 않을 것 같아 삭제

--- src/main/java/com/assignment/greengrocer/greengrocer/service/ExternalTokenService.java
+++ src/main/java/com/assignment/greengrocer/greengrocer/service/ExternalTokenService.java
@@ -3,22 +3,25 @@ package com.assignment.greengrocer.greengrocer.service;
 import com.assignment.greengrocer.greengrocer.external.GreengrocerType;
 import com.assignment.greengrocer.greengrocer.persistance.redis.ExternalToken;
 import com.assignment.greengrocer.greengrocer.persistance.redis.ExternalTokenRepository;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Slf4j
 @Service
-public class AsyncExternalTokenUpdateService {
+public class ExternalTokenService {
 
     private final ExternalTokenRepository repository;
 
-    @Async
     public void saveToken(GreengrocerType greengrocerType, String token) {
         ExternalToken externalToken = repository.save(new ExternalToken(greengrocerType, token));
         log.info("token save: {}", externalToken.getValue());
+    }
+
+    public Optional<ExternalToken> getTokenOptional(GreengrocerType greengrocerType) {
+        return repository.findById(greengrocerType);
     }
 
 }
