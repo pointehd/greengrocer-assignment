45b6bbf7b765623017c7309c062ee84ef2655fbf
Donghyun Hwang <pointehd@naver.com>
2023-02-01 01:24:35 +0900

    Feat: local cors 오류 수정
    - 로컬 cors 오류 수정

--- src/main/java/com/assignment/greengrocer/GreengrocerApplication.java
+++ src/main/java/com/assignment/greengrocer/GreengrocerApplication.java
@@ -3,6 +3,10 @@ package com.assignment.greengrocer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableConfigurationProperties
@@ -10,6 +14,18 @@ public class GreengrocerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GreengrocerApplication.class, args);
+	}
+
+
+	@Profile("local")
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+			}
+		};
 	}
 
 }
